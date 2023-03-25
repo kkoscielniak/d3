f1c421683466d4852a111fdf92ccc3571b8efd2f
Krystian Kościelniak <krystian@koscielniak.pro>
2023-03-25 22:24:01 +0100

    nvim: Update NvimTree focused file on enter buffer

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,33 +1,37 @@
-local setup, nvimTree = pcall(require, 'nvim-tree') 
+local setup, nvimTree = pcall(require, "nvim-tree")
 
-if not setup then 
-  return
-end 
+if not setup then
+	return
+end
 
 -- recommended settings from nvim-tree docs
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
--- colors 
+-- colors
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
 -- setup
 nvimTree.setup({
-  renderer = { 
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = "+", 
-          arrow_open = "-",
-        }
-      }
-    }
-  },
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false, 
-      },
-    },
-  },
-}) 
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "+",
+					arrow_open = "-",
+				},
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+})

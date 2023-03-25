9e64bf82687d08e3c22bce43939d7dd9758d6654
Krystian Kościelniak <krystian@koscielniak.pro>
2023-03-25 20:54:23 +0100

    sketchybar: use yabai space names directly

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -4,4 +4,11 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set $NAME background.drawing=$SELECTED
+## TODO: This should be configured in `sketchybarrc`
+# WINDOWS_COUNT=$(yabai -m query --windows --space $SID | jq length)
+
+# echo $WINDOWS_COUNT
+
+# if [[ $WINDOWS_COUNT -g#gcct 0 ]]; then
+  sketchybar --set $NAME background.drawing=$SELECTED;
+# fi

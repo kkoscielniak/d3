5e1a62ab6afa00e70418ed6c604aa293ca482534
Krystian Kościelniak <krystian@koscielniak.pro>
2023-03-25 21:31:58 +0100

    sketchybar: use `[]` instead of bg colorfor indicating the active space

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-
-## TODO: This should be configured in `sketchybarrc`
-# WINDOWS_COUNT=$(yabai -m query --windows --space $SID | jq length)
-
-# echo $WINDOWS_COUNT
-
-# if [[ $WINDOWS_COUNT -g#gcct 0 ]]; then
-  sketchybar --set $NAME background.drawing=$SELECTED;
-# fi
+if [[ $SELECTED = true ]]; then
+  sketchybar --set $NAME icon=[$NAME]
+else
+  sketchybar --set $NAME icon=$NAME
+fi

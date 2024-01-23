6eef90331b037b6c452db45f387044a46be7ae6b
Krystian Kościelniak <krystian@koscielniak.pro>
2024-01-23 11:48:40 +0100

    `sketchybar`: add fullscreen space indicator

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -2,17 +2,23 @@
 # $SELECTED
 # $NAME
 # $SID - space ID (numerical)
-echo "sid:" $SID;
+# $DID - display ID (unused)
 
-float_indicator=""
-type=$(yabai -m query --spaces --space $SID | jq '.type' | tr -d '"')
-if [ "$type" = "float" ]; then
-  float_indicator="*"
-fi
+get_space_indicator() {
+  type=$(yabai -m query --spaces --space $SID | jq '.type' | tr -d '"') # `bsp` | `float`
+  is_native_fullscreen=$(yabai -m query --spaces --space $SID | jq '."is-native-fullscreen"')
 
-## Get space mode in `yabai`:
+  # set indicator to `*` if the space is set to `float`
+  indicator=$([[ "$type" = "float" ]] && echo "*" || echo "")
+  
+  # set the indicator to `^` if the space is fullscreen
+  indicator=$([[ "$is_native_fullscreen" = "true" ]] && echo "^" || echo $indicator)
+
+  echo $indicator;
+}
+
 if [[ $SELECTED = true ]]; then
-  sketchybar --set $NAME icon="[$NAME$float_indicator]"
+  sketchybar --set $NAME icon="[$NAME$(get_space_indicator)]"
 else
-  sketchybar --set $NAME icon=$NAME$float_indicator
+  sketchybar --set $NAME icon=$NAME$(get_space_indicator)
 fi

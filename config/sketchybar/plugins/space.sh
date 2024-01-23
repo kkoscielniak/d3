fab046251ca094fc91b8c39d326b1d63e25ec294
Krystian Kościelniak <krystian@koscielniak.pro>
2024-01-23 11:11:48 +0100

    `sketchybar`: add "floating" space indicator

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/zsh
+# $SELECTED
+# $NAME
+# $SID - space ID (numerical)
+echo "sid:" $SID;
 
+float_indicator=""
+type=$(yabai -m query --spaces --space $SID | jq '.type' | tr -d '"')
+if [ "$type" = "float" ]; then
+  float_indicator="*"
+fi
+
+## Get space mode in `yabai`:
 if [[ $SELECTED = true ]]; then
-  sketchybar --set $NAME icon=[$NAME]
+  sketchybar --set $NAME icon="[$NAME$float_indicator]"
 else
-  sketchybar --set $NAME icon=$NAME
+  sketchybar --set $NAME icon=$NAME$float_indicator
 fi
